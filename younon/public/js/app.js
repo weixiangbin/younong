1b3570e0f3fae3763d32599b8c3d9ba7ee7065bf
weixiangbin <317505615@qq.com>
2015-09-21 17:38:50 +0800

    架构结束

--- younon/public/js/app.js
+++ younon/public/js/app.js
@@ -1,6 +1,6 @@
 // Ionic Starter App
 
-angular.module('starter', ['ionic', 'index.controllers', 'index.services'])
+angular.module('starter', ['ionic', 'index.controllers', 'index.services','admin.controllers', 'admin.services','person.controllers', 'person.services','shop.controllers', 'shop.services',])
 
 .run(function($ionicPlatform) {
   $ionicPlatform.ready(function() {
@@ -19,7 +19,8 @@ angular.module('starter', ['ionic', 'index.controllers', 'index.services'])
       .state('tab', {
     url: '/tab',
     abstract: true,
-    templateUrl: 'templates/index/tabs.html'
+    templateUrl: 'templates/index/tabs.html',
+    controller:'indexBaseCtrl'
   })
 
   .state('tab.index', {
@@ -59,7 +60,58 @@ angular.module('starter', ['ionic', 'index.controllers', 'index.services'])
                     controller: 'adminCtrl'
                 }
             }
-        });
+        })
+        //index路由结束
+
+
+      .state('shopping', {
+          url: '/shopping',
+          abstract: true,
+          controller:'shoppingBaseCtrl'
+      })
+
+      .state('shopping.index', {
+          url: '/index',
+          views: {
+                  templateUrl: 'templates/shopping/index.html',
+                  controller: 'shoppingCtrl'
+
+          }
+      })
+
+        //shopping路由结束
+
+      .state('person', {
+          url: '/person',
+          abstract: true,
+          controller:'personBaseCtrl'
+      })
+
+      .state('person.index', {
+          url: '/index',
+          views: {
+              templateUrl: 'templates/personInfor/index.html',
+              controller: 'personCtrl'
+
+          }
+      })
+
+      //person路由结束
+
+      .state('admin', {
+          url: '/admin',
+          abstract: true,
+          controller:'adminBaseCtrl'
+      })
+
+      .state('admin.index', {
+          url: '/index',
+          views: {
+              templateUrl: 'templates/adminstr/index.html',
+              controller: 'adminCtrl'
+
+          }
+      });
 
   $urlRouterProvider.otherwise('/tab/index');
 
