bd664473ec90a123ad04affa94c9f8a445ddb616
weixiangbin <317505615@qq.com>
2015-09-21 16:50:11 +0800

    项目架构修改

---
+++ younon/public/js/index/indexCtr.js
@@ -0,0 +1,20 @@
+angular.module('index.controllers', [])
+
+.controller('indexCtrl', function($scope) {
+        console.log('index');
+    })
+
+.controller('cartCtrl', function($scope) {
+        console.log('cart');
+
+})
+
+.controller('accountCtrl', function($scope) {
+        console.log('account');
+
+})
+
+.controller('adminCtrl', function($scope) {
+        console.log('admin');
+
+});
